b3d3c246282f22f52169cda3a1f5551b6333d023
Fredrik Ohlsson <fohlsson@fohlsson-macbookpro.roam.corp.google.com>
2017-08-05 20:26:54 +0200

    Updates to the html on the search page. Not done yet.

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -56,7 +56,22 @@ class SearchBooks extends Component {
           <ol className="books-grid">
           	{books.map((book) => (
           		<li key={book.id}>
-          			<div className="book-title">{book.title}</div>
+          			<div className='book'>
+          				<div className='book-top'>
+	          				<div className="book-cover"
+	          					style={{ width: 128, height: 193, backgroundImage: `url(${book.thumbnail})` }}></div>
+	          				<div className={`book-shelf-status ${book.shelf}`}></div>
+	          				<div className="book-shelf-changer">
+					            <select value={this.state.value} onChange={this.handleChange}>
+					              <option value="none" disabled>Move to...</option>
+					              <option value="currentlyReading">Currently Reading</option>
+					              <option value="wantToRead">Want to Read</option>
+					              <option value="read">Read</option>
+					              <option value="none">None</option>
+					            </select>
+					          </div>
+	          			</div>
+	          		</div>
           		</li>
           	))}
 
